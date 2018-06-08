2971c510f06b4d3cb3bfd56ffad17f1cf19c8ce5
Dylan Southard <kamisama42@gmail.com>
2018-06-07 21:35:47 -0700

    Initiate swipe view etc.

---
+++ FilmSwipe/SwipeView.swift
@@ -0,0 +1,42 @@
+//
+//  SwipeView.swift
+//  FilmSwipe
+//
+//  Created by Dylan Southard on 2018/06/07.
+//  Copyright © 2018 Dylan Southard. All rights reserved.
+//
+
+import UIKit
+
+class SwipeView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        self.commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("SwipeView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- FilmSwipe/ViewController.swift
+++ FilmSwipe/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import Koloda
+import pop
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var kolodaView: KolodaView!
+    
+    var images = [#imageLiteral(resourceName: "cat"),#imageLiteral(resourceName: "cat2"),#imageLiteral(resourceName: "cat3"),#imageLiteral(resourceName: "cat4"),#imageLiteral(resourceName: "cat5")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.kolodaView.delegate = self
+        self.kolodaView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,4 +31,45 @@ class ViewController: UIViewController {
 
 
 }
+
+extension ViewController : KolodaViewDelegate, KolodaViewDataSource {
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        //return UIImageView(image: self.images[index])
+        return SwipeView(frame: self.view.frame)
+        
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return 1000
+    }
+    
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        return .fast
+    }
+    
+    func koloda(_ koloda: KolodaView, didSwipeCardAt index: Int, in direction: SwipeResultDirection) {
+        switch direction {
+        case .right:
+            print("right")
+        case .left:
+            print("left")
+        case .bottomLeft:
+            print("bottom left")
+        case  .bottomRight:
+            print("bottom right")
+        case .topLeft:
+            print("top left")
+        case .topRight:
+            print("top right")
+        case .up:
+            print("up")
+        case .down:
+            print("down")
+        }
+        self.images.append(self.images[index])
+    }
+    
+}
+
+
 
